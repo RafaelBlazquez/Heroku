91fa25fd1d39bdc3eff0f225d6c75325cc1d5ed6
RafaelBlazquez <73467706+RafaelBlazquez@users.noreply.github.com>
2022-01-20 09:06:17 +0100

    Corregir autoload

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -4,7 +4,7 @@
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
 
-function __autoload($modelname){
+function spl_autoload_register($modelname){
 	if(Model::exists($modelname)){
 		include Model::getFullPath($modelname);
 	} 
