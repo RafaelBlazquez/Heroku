89fa4bc46779efefefe79f379396e76028ea1b7b
RafaelBlazquez <73467706+RafaelBlazquez@users.noreply.github.com>
2022-01-20 09:38:05 +0100

    Database 2.0

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -7,6 +7,7 @@ class Database {
 	}
 
 	function connect(){
+		$this->user="b230f0aa73f761";$this->pass="f7a6ed03";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_59f0b6996393531";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
